75bbf192dbb831ea34fcb650e8edf770aec61497
Ori Levi <orile03@gmail.com>
2024-01-28 14:01:44 +0200

    add new post button

--- facebook-iso-react/src/app/App.js
+++ facebook-iso-react/src/app/App.js
@@ -4,29 +4,29 @@ import articles from "../data/db.json"
 import { useState } from 'react';
 
 function App() {
-const [articlesList, setArticleList] = useState(articles)
-console.log(articlesList)
-var i = 1
-console.log(i)
-i+=1
-console.log(i)
-const addArticle = () => {
-  const article = {
+  const [articlesList, setArticleList] = useState(articles);
+
+  const addArticle = () => {
+    const article = {
       "id" : 3,
       "title" : "another post",
       "author" : "Someone Else",
       "category" : "Food",
       "publication_date" : "27/1/2024"
+    }
+    setArticleList([...articlesList, article])
   }
-  setArticleList([...articlesList, article])
-}
 
   return (
     <div className="App">
-      <button onClick={addArticle}>Add</button>
-      {
-        articlesList.map((articles) => <Article {...articles} />)
-      }
+      <div className="content">
+        {
+          articlesList.map((articles) => <Article {...articles} />)
+        }
+      </div>
+      <div className="button-container">
+        <button onClick={addArticle}><h1>NEW POST</h1></button>
+      </div>
     </div>
   );
 }

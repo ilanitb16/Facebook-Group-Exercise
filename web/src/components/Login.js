d5b82bc75e50e2dff464c0d27ba3a9930fc39c64
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-01-29 14:52:45 +0200

    added basic design, simple login logic

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,40 +1,56 @@
 import React, { useState } from 'react';
+import { authenticateUser } from '../Auth';
+import '../styles/Login.css'; // Import the CSS file
+
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false); // flag to track login status
 
   const handleLogin = () => {
-    // Implement login logic here.
-    // Logging the input values.
-    console.log(`Username: ${username}`);
-    console.log(`Password: ${password}`);
+    // Add login logic here.
+    const user = authenticateUser(username, password);
+
+    if (user) {
+        // Authentication successful
+        setLoggedIn(true);
+      } else {
+        // Authentication failed
+        alert('Incorrect username or password. Please try again.');
+      }
   };
 
   return (
     <div className="login-container">
-      <h2>Login</h2>
-      <form>
-        <div className="form-group">
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
+      {loggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <>
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <label>Username:</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 };

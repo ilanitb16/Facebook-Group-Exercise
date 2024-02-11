3adefd541884d61163a465382de03db3f867da3d
Ori Levi <orile03@gmail.com>
2024-02-11 22:49:39 +0200

    start merge feed to main

--- facebook-iso-react/src/conector/Connector.test.js
+++ facebook-iso-react/src/conector/Connector.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Connector from './Connector';
 
 test('check if hello world exists', () => {
-  render(<App />);
+  render(<Connector />);
   const linkElement = screen.getByText(/Hello world/i);
   expect(linkElement).toBeInTheDocument();
 });

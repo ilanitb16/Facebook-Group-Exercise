50399909640be5bded348d5ab7139a0a29f76501
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-03-12 14:22:38 +0200

    improve info

--- client-react/src/components/Info/Info.js
+++ client-react/src/components/Info/Info.js
@@ -1,16 +1,22 @@
+81% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for ₪8.00 ₪2.00/month for 3 months.
+import {Link} from "react-router-dom";
+import { useUser } from '../../providers/user_context';
+
 function Info({ user_name, user_photo }) {
-    return (
-      <div className="info col-2 vh-100">
-        <ul className="list-group">
-          <span className="profile-info">
-            <h3>
-              <img src={user_photo} className="img-profile-user" alt="..." />
-              <b className="user-name">{"@" + user_name}</b>
-            </h3>
-          </span>
-        </ul>
-      </div>
-    );
+  const [user, setUser] = useUser();
+
+  return (
+    <div className="info col-2 vh-100">
+      <ul className="info-container">
+        <Link to={`/${user.username}`} className="profile-info">
+          <h3>
+            <img src={user_photo} className="img-profile-user" alt="..." />
+            <b className="user-name">{"@" + user_name}</b>
+          </h3>
+        </Link>
+      </ul>
+    </div>
+  );
   }
   
   export default Info;

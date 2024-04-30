afd9069799d095acb26d6dd926ec88832e4c9e90
ori2236 <131310484+ori2236@users.noreply.github.com>
2024-04-30 11:22:25 +0300

    Revert "Server part4"

--- server/controllers/updatePostController.js
+++ server/controllers/updatePostController.js
@@ -2,31 +2,6 @@ const {ok} = require("../utils");
 const PostReq = require("../models/postReqModel");
 const { ObjectId } = require("mongodb");
 
-// Function to communicate with the TCP server
-function communicateWithTCPServer(data) {
-    return new Promise((resolve, reject) => {
-        const client = net.createConnection({ port: 5555 }, () => {
-            console.log('Connected to TCP server');
-            client.write(data);
-        });
-        
-        client.on('data', (response) => {
-            console.log('Received from TCP server:', response.toString());
-            client.end();
-            resolve(response.toString());
-        });
-        
-        client.on('end', () => {
-            console.log('Disconnected from TCP server');
-        });
-        
-        client.on('error', (err) => {
-            console.error('Error communicating with TCP server:', err);
-            reject(err);
-        });
-    });
-}
-
 module.exports.updatePostController = async (request, response, next) => {
     try{
         let result;
@@ -39,35 +14,7 @@ module.exports.updatePostController = async (request, response, next) => {
             post.username = postRequest.username
         }
         if(postRequest.description){
-        // Regular expression to match URLs with "https://www.[any].com"
-            const urlRegex = /https:\/\/www\.[^.]+\.com/g;
-            const matches = postRequest.description.match(urlRegex);
-
-            if (matches && matches.length > 0) {
-                console.log("Found matching URLs:", matches);
-                
-                // Send each match to the TCP server for validation
-                for (let match of matches) {
-                    const regexToSend = "2 " + match;
-                    try {
-                        const validationResponse = await communicateWithTCPServer(regexToSend);
-                        if (validationResponse) {
-                            // If the TCP server returns '1' at the beginning of the response, it means the post should not be uploaded
-                            result = { status: 400, result: { message: "Post cannot be updated due to validation failure." } };
-                            ok(response, result);
-                            return;
-                        } else {
-                            post.description = postRequest.description;
-                        }
-                    } catch (error) {
-                        // Handle communication error with the TCP server
-                        console.error("Error communicating with TCP server:", error);
-                        result = { status: 500, result: { message: "Error communicating with TCP server." } };
-                        ok(response, result);
-                        return;
-                    }
-                }
-            }
+            post.description = postRequest.description
         }
         if(postRequest.img){
             post.img = postRequest.img
